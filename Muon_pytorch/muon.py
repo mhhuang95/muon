a6c0d5d7efec1f6ab116f54702d1b38d62332038
mhhuang95 <mhhuang@ucdavis.edu>
2025-01-16 23:55:13 -0800

    muon implementation

---
+++ Muon_pytorch/muon.py
@@ -0,0 +1,128 @@
+import torch
+import torco.distributed as dist
+
+@torch.compile
+def newtonschulz5(G, steps):
+    assert len(G.shape) == 2
+    a, b, c = (3.4445, -4.7750, 2.0315)
+    X = G.bfloat16()
+    if G.size(0) > G.size(1):
+        X = X.T
+
+    X = X / (X.norm() + 1e-7)
+
+    for _ in range(steps):
+        A = X @ X.T
+        B = b * A + c * A @ A
+        X = a * X + B @ X
+
+    if G.size(0) > G.size(1):
+        X = X.T
+    
+    return X
+
+class Muon(torch.optim.Optimizer):
+    def __init__ (
+        self, 
+        muon_params, 
+        lr=0.02, 
+        momuntum=0.95, 
+        nesterov=True,
+        ns_steps=6,
+        adamw_params=None,
+        adamw_lr=3e-4,
+        adamw_betas=(0.95, 0.95),
+        adamw_eps=1e-8,
+        adamw_wd=0,
+    ):
+        defaults = dict(lr=lr, momuntum=momuntum, nesterov=nesterov, ns_steps=ns_steps,
+                        adamw_lr_ratio=adamw_lr/lr, adamw_betas=adamw_betas,
+                        adamw_eps=adamw_eps, adamw_wd=adamw_wd)
+        
+        params = list(muon_params)
+        adamw_params = list(adamw_params) if adamw_params is not None else []
+        params.extend(adamw_params)
+        super().__init__(params, defaults)
+
+        for p in muon_params:
+            if p.ndim >= 2:
+                self.state[p]['use_muon'] = True
+            else:
+                self.state[p]['use_muon'] = False
+        for p in adamw_params:
+            self.state[p]['use_muon'] = False
+        
+
+    def step(self, closure=None):
+
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+        
+        for group in self.param_groups:
+
+            ################################
+            #            Muon              #
+            ################################
+
+            params = [p for p in group['params'] if self.state[p]['use_muon']]
+            lr = group['lr']
+            momentum = group['momentum']
+
+            for p in params:
+                g = p.grad
+                if g is None:
+                    continue
+                if g.ndim > 2:
+                    g = g.view(g.size(0), -1)
+                assert g is not None
+                state = self.state[p]
+                if 'momentum_buffer' not in state:
+                    state['momentum_buffer'] = torch.zeros_like(g)
+                buf = state['momentum_buffer']
+                buf.mul_(momentum).add_(g)
+                if group['nesterov']:
+                    g = g.add(buf, alpha=momentum)
+                else:
+                    g = buf
+                g = newtonschulz5(g, steps=group['ns_steps'])
+                g *= max(1, g.size(0)/g.size(1))**5
+                
+                p.data.add_(g, alpha=-lr)
+            
+            ################################
+            #            AdamW             #
+            ################################
+            
+            params = [p for p in group['params'] if not self.state[p]['use_muon']]
+            lr = group['adamw_lr_ratio'] * group['lr']
+            beta1, beta2 = group['adamw_betas']
+            eps = group['adamw_eps']
+            weight_decay = group['adamw_wd']
+
+            for p in params:
+                g = p.grad
+                if g is None:
+                    continue
+                state = self.state[p]
+                if 'step' not in state:
+                    state['step'] = 0
+                    state['moment1'] = torch.zeros_like(g)
+                    state['moment2'] = torch.zeros_like(g)
+                state['step'] += 1
+                step = state['step']
+                buf1 = state['moment1']
+                buf2 = state['moment2']
+                buf1.lerp_(g, 1-beta1)
+                buf2.lerp_(g.square(), 1-beta2)
+
+                g = buf1 / (eps + buf2.sqrt())
+
+                bias_correction1 = 1 - beta1**step
+                bias_correction2 = 1 - beta2**step
+                scale = bias_correction1 / bias_correction2**0.5
+                p.data.mul_(1 - lr * weight_decay)
+                p.data.add_(g, alpha=-lr/scale)
+                
+        return loss
